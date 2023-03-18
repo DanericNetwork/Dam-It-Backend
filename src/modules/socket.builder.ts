cf0374e208aa216ca5c12410c1233f4483a94864
martvdm <martvandermolen@gmail.com>
2023-03-18 17:00:17 +0100

    refactor: socketbuilder function parameter type

--- src/modules/socket.builder.ts
+++ src/modules/socket.builder.ts
@@ -1,9 +1,9 @@
 export default class Websocket {
-    public name: string;
-    public function: any;
+  public name: string;
+  public function: Function;
 
-    constructor(name: string, func: any) {
-        this.name = name;
-        this.function = func;
-    }
+  constructor(name: string, func: Function) {
+    this.name = name;
+    this.function = func;
+  }
 }
