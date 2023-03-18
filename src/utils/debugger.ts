05b8a43cf48c2af85c8eef63bd96c0a9f43abcfa
martvdm <martvandermolen@gmail.com>
2023-03-18 17:02:56 +0100

    refactor: removed unused comment

--- src/utils/debugger.ts
+++ src/utils/debugger.ts
@@ -1,4 +1,3 @@
-// Make a function that only console.logs if the environment is development
 export default function Debug(...args: any[]) {
   if (process.env.NODE_ENV === "development") {
     console.log(...args);
