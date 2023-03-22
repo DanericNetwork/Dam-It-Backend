bbc06ab2e23532f6f6feebe6de973bc9aaf638c8
Martvdm <martvandermolen@gmail.com>
2023-03-22 10:17:11 +0100

    refactor: prettier (2)

--- src/modules/room/room.service.ts
+++ src/modules/room/room.service.ts
@@ -1,13 +1,13 @@
 import { randomInt } from "crypto";
 
 export class Room {
-    pin: number;
-    author: string;
-    created_at: Date;
+  pin: number;
+  author: string;
+  created_at: Date;
 
-    constructor(author: string) {
-        this.pin =  randomInt(100000, 999999);
-        this.author = author;
-        this.created_at = new Date();
-    }
+  constructor(author: string) {
+    this.pin = randomInt(100000, 999999);
+    this.author = author;
+    this.created_at = new Date();
+  }
 }
--- src/modules/socket.builder.ts
+++ src/modules/socket.builder.ts
@@ -2,7 +2,7 @@
  * Builds an websocket for socket.ts files.
  * @param {string} name represents the socket ($io.emit("name"))
  * @param {any} function executed when socket is emitted (with parameters)
- * @example new Websocket("test", () => { console.log("this is a test") }) 
+ * @example new Websocket("test", () => { console.log("this is a test") })
  **/
 export default class Websocket {
   public name: string;
