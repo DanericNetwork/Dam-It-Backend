2d8407be13f511cfcc6b7469c936ddf40a2af3ae
Martvdm <martvandermolen@gmail.com>
2023-03-22 12:55:45 +0100

    docs: websocket class functions

--- src/modules/socket.builder.ts
+++ src/modules/socket.builder.ts
@@ -1,34 +1,54 @@
 import { Socket } from "socket.io";
 
 export default class WebSocket {
-  private _name: string = '';
+  private _name: string = "";
   private _execution: Function = () => {};
-  private _client: any = null;
+  private _client: Socket = {} as Socket;
 
   constructor() {
     this._name = this.constructor.name.toLowerCase();
   }
 
+  /**
+   * Sets the name of the socket
+   **/
   setName(name: string): void {
     this._name = name;
   }
 
-  setExecution(execution: Function): void {
+  /**
+   *  Sets the function to be executed when the socket is called
+   * @param {Function} execution The function to be executed when the socket is called
+   **/
+  setExecution(execution: Function | any): void {
     this._execution = execution;
   }
 
+  /**
+   * Sets the client of the socket
+   * @param {Socket} client The client of the socket
+   **/
   setClient(client: Socket): void {
     this._client = client;
   }
 
+  /**
+   * Get the name of the socket
+   **/
   get name(): string {
     return this._name;
   }
 
-  get execution(): any {
+  /**
+   * Get the function to be executed when the socket is called
+   **/
+  get execution(): Function | any {
     return this._execution;
   }
 
+  /**
+   * Get the client of the socket
+  **/
   get client(): Socket {
     return this._client;
   }
