d9947b9a2edbfcba48f2452ba027e76a72300c83
Daneric <danericaalbers@hotmail.com>
2023-04-12 11:32:07 +0200

    feat: initialize mongoserver

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,13 @@
 import ExpressServer from "./server/express.server";
 import SocketServer from "./server/socket.server";
+import MongoServer from "./server/mongo.server";
 
 /** Create the servers **/
 export const expressServer: ExpressServer = new ExpressServer();
 export const socketServer: SocketServer = new SocketServer();
+export const mongoServer: MongoServer = new MongoServer();
 
 /** Start the servers **/
 expressServer.start();
 socketServer.start();
+mongoServer.start();
